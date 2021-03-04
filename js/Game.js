307d7615ff93dbd85bd85ed4c309a3ae8685292c
adwitasinha <71048895+adwitasinha@users.noreply.github.com>
2021-03-03 16:56:50 -0800

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,56 @@
+class Game {
+  constructor(){
+
+  }
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){ 
+    if(gameState === 0){ 
+      player = new Player(); 
+      var playerCountRef = await database.ref('playerCount').once("value"); 
+      if(playerCountRef.exists()){ 
+        playerCount = playerCountRef.val(); 
+        player.getCount(); 
+      } 
+      form = new Form() 
+      form.display(); 
+} }
+
+  play(){
+form.hide();
+textSize(18);
+text("game start", 150, 100);
+Player.getPlayerinfo()
+if (allPlayers!==undefined) {
+  var displayPosition = 160;
+  for (var plr in allPlayers) {
+    if (plr === "player" + player.index)
+      fill("green");
+      else 
+      fill("red");
+      displayPosition += 20;
+      textSize(20);
+      text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,displayPosition);
+  }
+}
+
+if (keyDown(UP_ARROW) && player.index!==null) {
+  player.distance += 50;
+  player.update();
+}
+
+  }
+}
